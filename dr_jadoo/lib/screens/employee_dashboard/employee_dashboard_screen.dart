ee41e139acbd81a5cf18507fb764f1e15c518f62
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2022-09-03 07:40:33 +0530

    - get current employee details

--- dr_jadoo/lib/screens/employee_dashboard/employee_dashboard_screen.dart
+++ dr_jadoo/lib/screens/employee_dashboard/employee_dashboard_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dr_jadoo/model/User/current_user.dart';
+import 'package:dr_jadoo/services/employee_dashboard_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,19 @@ class EmployeeDashboardScreen extends StatefulWidget {
 }
 
 class _EmployeeDashboardScreenState extends State<EmployeeDashboardScreen> {
+  late CurrentUser currentUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initstate();
+  }
+
+  void initstate() async {
+    currentUser = await EmployeeDashboardSerivce.instance.getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
